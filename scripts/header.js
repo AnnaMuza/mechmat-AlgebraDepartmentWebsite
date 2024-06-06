972b40df09cac9e3f7b9c405cbc2d9e7c65b8de4
Anna Muzychyna <annamuz69@gmail.com>
2024-06-06 14:42:02 +0300

    WIP

--- scripts/header.js
+++ scripts/header.js
@@ -1,57 +1,67 @@
 $(document).ready(function () {
+  window.onscroll = function() {scrollFunction()};
   changeTab();
-  addEventListener("hashchange", function () {
+  addEventListener("hashchange", function() {
     changeTab();
   });
 });
 
+const LANG_REGEX = /\/(uk|en)\//;
+
 function redirectToHome() {
-  window.location.hash = '#professors';
+  window.location.hash = '#history';
+}
+
+function changeLang(lang) {
+  window.location.href = window.location.href.replace(LANG_REGEX, `/${lang}/`);
 }
 
 function changeTab() {
   const url = window.location;
   const hash = url.hash;
-  const lang = url.href.match(/\/(uk|en)\//)?.[1];
+  const lang = url.href.match(LANG_REGEX)?.[1];
   if (!lang || !hash) redirectToHome();
-  $(`a[href="${hash}"]`).css('color', '#fff');
-  $('#content').load(`../${lang}/pages/${hash.slice(1)}.html`);
+  const altLang = lang === 'en' ? 'uk' : 'en';
+  const langButton = $(`#lang_${altLang}`);
+  langButton.click(function() {changeLang(altLang)});
+  langButton.css('cursor', 'pointer');
+  const folder = (hash.match(/-/g) || []).length === 2 ? 'cv' : 'pages';
+  $('#navbar a').css('color', '');
+  $(`a[href="${folder === 'cv' ? '#professors' : hash}"]`).css('color', '#fff');
+  $('#content').load(`../${lang}/${folder}/${hash.slice(1)}.html`);
+  window.scrollTo(0, 0);
 }
 
-window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
-  if (window.matchMedia("(min-width: 480px)").matches) {
+  if (window.matchMedia('(min-width: 480px)').matches) {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       $("#header_kaf").height(0);
-      document.getElementById("header_list").style = "top: 0;"
-      document.getElementById("name").style = "margin-top: 0; opacity: 0;";
-      document.getElementById("meh").style = "opacity: 0; transition: all 0.2s ease;";
-      document.getElementById("meh_knu").style = "opacity: 0;";
-      document.getElementById("lang").style = "opacity: 0; transition: all 0.2s ease;";
-      document.getElementById("kaf_logo").style = "opacity: 1;";
+      $("#header_list").css({top: 0});
+      $("#name").css({marginTop: 0, opacity: 0});
+      $("#meh").css({opacity: 0, transition: "all 0.2s ease"});
+      $("#meh_knu").css({opacity: 0});
+      $("#lang").css({opacity: 0, transition: "all 0.2s ease"});
+      $("#kaf_logo").css({opacity: 1});
     } else {
-      document.getElementById("header_kaf").style.height = "100px";
-      document.getElementById("header_list").style = "top: 100px;"
-      document.getElementById("name").style = "margin-top: 30px; opacity: 1;";
-      document.getElementById("meh").style = "opacity: 1; transition: all 0.4s ease; transition-delay: 0.1s;";
-      document.getElementById("meh_knu").style = "opacity: 1;";
-      document.getElementById("lang").style = "opacity: 1; transition: all 0.4s ease; transition-delay: 0.1s;";
-      document.getElementById("kaf_logo").style = "opacity: 0;";
+      $("#header_kaf").css({height: "100px"});
+      $("#header_list").css({top: "100px"});
+      $("#name").css({marginTop: "30px", opacity: 1});
+      $("#meh").css({opacity: 1, transition: "all 0.4s ease", transitionDelay: "0.1s"});
+      $("#meh_knu").css({opacity: 1});
+      $("#lang").css({opacity: 1, transition: "all 0.4s ease", transitionDelay: "0.1s"});
+      $("#kaf_logo").css({opacity: 0});
     }
   }
 }
 
-document.getElementById("menu_open").onclick = function() {
-  document.getElementById("header_list").style.top = "80px";
-  document.getElementById("menu_open").style = 'display: none;';
-  document.getElementById("menu_close").style = 'display: block;';
+$("#menu_open").click(function() {
+  $("#header_list").css({top: "80px"});
+  $("#menu_open").css({display: 'none'});
+  $("#menu_close").css({display: 'block'});
+});
 
-};
-
-document.getElementById("menu_close").onclick = function() {
-  document.getElementById("header_list").style.top = "-300px";
-  document.getElementById("menu_close").style = 'display: none;';
-  document.getElementById("menu_open").style = 'display: block;';
-
-};
+$("#menu_close").click(function() {
+  $("#header_list").css({top: "-300px"});
+  $("#menu_close").css({display: 'none'});
+  $("#menu_open").css({display: 'block'});
+});
